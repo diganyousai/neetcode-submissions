3e410c500baf753bbe2345e8b85df8eef877b9b8
diganyousai <2186698608@qq.com>
2026-08-24 00:57:28 +0800

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hashmap = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        res = []
+        path = []
+
+        def dfs(i):
+            if i == len(digits):
+                res.append(''.join(path))  #不需要去重，没有重复
+                return
+
+            for c in hashmap[digits[i]]:   #在for loop里直接回溯
+                path.append(c)
+                dfs(i + 1)
+                path.pop()
+
+        if digits:
+            dfs(0)
+
+        return res
