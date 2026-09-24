99506117e545b6f116476fc156d75cfab61059ca
diganyousai <2186698608@qq.com>
2026-09-24 18:14:03 +0800

    Add: recover-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/recover-binary-search-tree/submission-0.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        arr = []
+        def inorder(node):
+            if not node:
+                return
+
+            inorder(node.left)
+            arr.append(node)
+            inorder(node.right)
+
+        inorder(root)
+        node1, node2 = None, None
+
+        for i in range(len(arr) - 1):  #中序遍历只有两个交换了，中间碰到正常大小关系的都直接跳过
+            if arr[i].val > arr[i + 1].val:
+                node2 = arr[i + 1]   #记得是后面位置出现的小的
+                if node1 is None:
+                    node1 = arr[i]   #记得是前面位置出现的大的
+                else:
+                    break
+        node1.val, node2.val = node2.val, node1.val  #交换两者
